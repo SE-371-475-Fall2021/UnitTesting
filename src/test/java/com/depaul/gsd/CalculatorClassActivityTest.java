031f117693fe64e6ca50a25aea834092a062f30e
salemij93 <salemi-j@hotmail.com>
2021-10-06 19:40:55 -0500

    Added test methods for getValue, add, subtract

---
+++ src/test/java/com/depaul/gsd/CalculatorClassActivityTest.java
@@ -0,0 +1,31 @@
+package com.depaul.gsd;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorClassActivityTest {
+    CalculatorClassActivity calculatorClassActivity;
+
+    @BeforeEach
+    void setUp() {
+         calculatorClassActivity = new CalculatorClassActivity();
+    }
+    @Test
+    void getValue() {
+        assertEquals(0,calculatorClassActivity.getValue());
+    }
+
+    @Test
+    void add(){
+        calculatorClassActivity.add(7);
+        assertEquals(7,calculatorClassActivity.getValue());
+    }
+
+    @Test
+    void subtract(){
+        calculatorClassActivity.subtract(10);
+        assertEquals(-10,calculatorClassActivity.getValue());
+    }
+}
